400b7ac26ef5c042566c7a0abb03a4e5b4006a5e
Gabriel Ruizz <garciagabriel1945@gmail.com>
2022-03-30 14:12:13 -0700

    home and nav componets added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Background from "./img/background.jpg";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ backgroundImage: `url(${Background})` }}>
       <Router>
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </Router>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Box } from "@material-ui/core";
+
+function Home() {
+  return (
+    <div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          "& > *": {
+            m: 1,
+          },
+        }}
+      >
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>gg</h1>
+        <h4>Portfolio</h4>
+      </Box>
+    </div>
+  );
+}
+
+export default Home;
